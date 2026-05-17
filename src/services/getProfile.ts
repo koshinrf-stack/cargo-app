7d9954470388b77e2a4d84e3d192b354963cd55b
Alexey Putilin <alexey@MacBook-Air-Alexey.local>
2026-05-18 02:55:25 +0800

    add getProfile service

---
+++ src/services/getProfile.ts
@@ -0,0 +1,26 @@
+import { supabase } from "@/lib/supabase";
+
+export async function getProfile(
+  telegramId: number
+) {
+
+  const { data, error } =
+    await supabase
+
+      .from("users")
+
+      .select("*")
+
+      .eq(
+        "telegram_id",
+        telegramId
+      )
+
+      .maybeSingle();
+
+  return {
+    data,
+    error,
+  };
+
+}
